4bd4b1800c382c36d40a7835ab3df01a59252c8d
daiki <morilr.tabletennis@gmail.com>
2020-12-19 15:33:06 +0900

    active_strage導入 userモデルに追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     added_attrs = [ :email, :username, :password, :password_confirmation ]
-    appdate_attrs = [ :email, :username, :introduction, :password, :password_confirmation ]
+    appdate_attrs = [ :avatar, :email, :username, :introduction, :password, :password_confirmation ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: appdate_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
