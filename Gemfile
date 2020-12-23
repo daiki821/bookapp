58b73e2076dac6d380f6b399808aa043acf1afdd
daiki <morilr.tabletennis@gmail.com>
2020-12-23 21:08:02 +0900

    kaminari導入

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'haml-rails'
 gem 'devise'
 gem 'font-awesome-sass'
+gem 'kaminari'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,13 @@ class TasksController < ApplicationController
 
   # まだ未完了のタスク一覧
   def todo
-    @todo = current_user.tasks.where(completed: 'false')
+    todo = current_user.tasks.where(completed: 'false')
+    @todo = todo.page(params[:page]).per(6)
   end
   # 完了したタスク一覧
   def done
-    @done = current_user.tasks.where(completed: 'true')
+    done = current_user.tasks.where(completed: 'true')
+    @done = done.page(params[:page]).per(6)
   end
 
   def new
@@ -17,7 +19,7 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      redirect_to tasks_path, notice: '保存できました'
+      redirect_to todo_tasks_path, notice: '保存できました'
     else
       flash.now[:error] = '保存できませんでした'
       render :new
@@ -31,7 +33,7 @@ class TasksController < ApplicationController
   def update
     @task = current_user.tasks.find(params[:id])
     if @task.update(task_params)
-      redirect_to tasks_path, notice: '更新できました'
+      redirect_to todo_tasks_path, notice: '更新できました'
     else
       flash.now[:error] = '更新できませんでした'
       render :edit
