c6bf0deb483690d46be5520a764f91c656471a9a
daiki <morilr.tabletennis@gmail.com>
2021-01-19 22:24:47 +0900

    トップページの作成

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+  before_action :redirect_user
+  def home
+  end
+
+
+  private
+  def redirect_user
+    redirect_to todo_tasks_path if user_signed_in?
+  end
+end
