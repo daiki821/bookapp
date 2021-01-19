e57512bf34bfbdd24f6d52d1834b85f56021bb1d
daiki <morilr.tabletennis@gmail.com>
2021-01-19 22:24:19 +0900

    トップページ作成によりルーティングの変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   def done
     task = current_user.tasks.find(params[:id])
     @task = task.update(completed: 'true')
-    redirect_to root_path
+    redirect_to todo_tasks_path
   end
 
   def new
@@ -25,7 +25,7 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      redirect_to root_path, notice: '保存できました'
+      redirect_to todo_tasks_path, notice: '保存できました'
     else
       flash.now[:error] = '保存できませんでした'
       render :new
@@ -39,7 +39,7 @@ class TasksController < ApplicationController
   def update
     @task = current_user.tasks.find(params[:id])
     if @task.update(task_params)
-      redirect_to root_path, notice: '更新できました'
+      redirect_to todo_tasks_path, notice: '更新できました'
     else
       flash.now[:error] = '更新できませんでした'
       render :edit
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
     @task = current_user.tasks.find(params[:id])
     @task.destroy!
     if @task.completed == false
-      redirect_to root_path, notice: '削除しました'
+      redirect_to todo_tasks_path, notice: '削除しました'
     else
       redirect_to finish_tasks_path, notice: '削除しました'
     end
