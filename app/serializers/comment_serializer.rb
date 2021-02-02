95f0fbd34d5f34e0efa9ba895739fb0143c7c39e
daiki <morilr.tabletennis@gmail.com>
2021-02-02 18:02:35 +0900

    ajaxで画像が表示されないため修正

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -31,7 +31,7 @@ class CommentSerializer < ActiveModel::Serializer
     if object.user.avatar.attached?
       url_for(object.user.avatar)
     else
-      '/assets/default_avatar.png'
+      '/images/ajax_default_avatar.png'
     end
   end
 
